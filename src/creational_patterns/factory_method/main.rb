648d9336cec5de8b4e1a02d4316f56f5e38f4e6a
takaakit <takaaki.teshima.dev@gmail.com>
2022-04-02 07:50:19 +0900

    Fixed typo

--- src/creational_patterns/factory_method/main.rb
+++ src/creational_patterns/factory_method/main.rb
@@ -3,7 +3,7 @@
 require_relative 'credit_card/credit_card_factory'
 
 =begin
-The subject is a factory to make credit cards. The Factory defines how to create an credit card,
+The subject is a factory to make credit cards. The Factory defines how to create a credit card,
 but the actual credit card is created by the CreditCardFactory.
 The "createProduct()" is called a Factory Method, and it is responsible for manufacturing an object.
 =end
